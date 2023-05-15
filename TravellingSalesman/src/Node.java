75472af71bf7e5160a0c59999214a2f7e408db6f
27134003 <27134003@L829WD210020.edu.cbe.ab.ca>
2023-05-15 13:15:37 -0600

    Added node and utility classes

---
+++ TravellingSalesman/src/Node.java
@@ -0,0 +1,26 @@
+
+public class Node {
+	private String name;
+	private double xPosition;
+	private double yPosition;
+	
+	
+	public Node (String name, double xPosition, double yPosition) {
+		this.name = name;
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+	}
+	
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getXPosition() {
+		return this.xPosition;
+	}
+	
+	public double getYPosition() {
+		return this.yPosition;
+	}
+}
---
+++ TravellingSalesman/src/utilities.java
@@ -0,0 +1,15 @@
+
+public class utilities {
+	
+	
+	public static double pythagoras(double pointOneX, double pointOneY, double pointTwoX, double pointTwoY) {
+		double base = pointOneX - pointTwoX;
+		double height = pointOneY - pointTwoY;
+		return pythagoras(base, height);
+	}
+	
+	public static double pythagoras(double base, double height) {
+		return Math.hypot(base, height);
+//		return Math.sqrt((height*height)+(base*base));
+	}
+}
