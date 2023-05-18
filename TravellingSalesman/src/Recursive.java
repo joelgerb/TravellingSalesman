a826b73a207a061258f174fcf1166a4a289d4a6e
27134003 <27134003@L829WD210020.edu.cbe.ab.ca>
2023-05-18 14:59:48 -0600

    recursive edit

--- TravellingSalesman/src/Recursive.java
+++ TravellingSalesman/src/Recursive.java
@@ -1,49 +1,83 @@
 import java.util.ArrayList;
 
-public class Recursive {
-	public void solve(ArrayList<Node> nodes) {
-		double distance = 0;
-		double bestDistance = Double.POSITIVE_INFINITY;
-		System.out.println("I'm Solving using recursion!");
-		
-		
-		for (int i = 0; i < nodes.size(); i++) {
-			
-			
-			solve(nodes, nodes.get(i), distance, bestDistance);
-			nodes.get(i).setVisited(true);
-			
-			if (distance < bestDistance) {
-				bestDistance = distance;
-			}
-			distance = 0;
 
+public class Recursive {
+	public static void printAllRecursive(int n, ArrayList<Node> input) {
+		ArrayList<Node> nodes = (ArrayList<Node>) input.clone();
 		
-			//im trying to figure this out
-//			distance += Utilities.distance(previousNode.getX, previousNode.getY, ode.getX, node.getY)
-//			solve(nodes, distance);
-		}
-		for (int i = 0; i < nodes.size(); i++) {
-			nodes.get(i).setVisited(false);
-		}
-		
+	    if(n == 1) {
+	        
+	    } else {
+	        for(int i = 0; i < n-1; i++) {
+	            printAllRecursive(n - 1, nodes);
+	            if(n % 2 == 0) {
+	                swap(nodes, i, n-1);
+	            } else {
+	                swap(nodes, 0, n-1);
+	            }
+	        }
+	        printAllRecursive(n - 1, nodes);
+	    }
 	}
 	
-	public void solve(ArrayList<Node> nodes, Node lastNode, double distance, double bestDistance) {
-		for (int i = 0; i < nodes.size(); i++) {
-			
-			if (nodes.get(i).getVisited() == false) {
-				distance += Utilities.distance(lastNode, nodes.get(i));
-				solve(nodes, nodes.get(i), distance, bestDistance);
-				nodes.get(i).setVisited(true);
-			}
-			
-			//im trying to figure this out
-//			distance += Utilities.distance(previousNode.getX, previousNode.getY, ode.getX, node.getY)
-//			solve(nodes, distance);
-		}
-		
+	private static void swap(ArrayList<Node> nodes, int a, int b) {
+	    Node temp = nodes.get(a);
+	    nodes.set(a, nodes.get(b));
+	    nodes.set(b, temp);
 	}
-	
-	
 }
+
+
+
+
+
+
+
+
+//public class Recursive {
+//	public void solve(ArrayList<Node> nodes) {
+//		double distance = 0;
+//		double bestDistance = Double.POSITIVE_INFINITY;
+//		System.out.println("I'm Solving using recursion!");
+//		
+//		
+//		for (int i = 0; i < nodes.size(); i++) {
+//			
+//			
+//			solve(nodes, nodes.get(i), distance, bestDistance);
+//			nodes.get(i).setVisited(true);
+//			
+//			if (distance < bestDistance) {
+//				bestDistance = distance;
+//			}
+//			distance = 0;
+//
+//		
+//			//im trying to figure this out
+////			distance += Utilities.distance(previousNode.getX, previousNode.getY, ode.getX, node.getY)
+////			solve(nodes, distance);
+//		}
+//		for (int i = 0; i < nodes.size(); i++) {
+//			nodes.get(i).setVisited(false);
+//		}
+//		
+//	}
+//	
+//	public void solve(ArrayList<Node> nodes, Node lastNode, double distance, double bestDistance) {
+//		for (int i = 0; i < nodes.size(); i++) {
+//			
+//			if (nodes.get(i).getVisited() == false) {
+//				distance += Utilities.distance(lastNode, nodes.get(i));
+//				solve(nodes, nodes.get(i), distance, bestDistance);
+//				nodes.get(i).setVisited(true);
+//			}
+//			
+//			//im trying to figure this out
+////			distance += Utilities.distance(previousNode.getX, previousNode.getY, ode.getX, node.getY)
+////			solve(nodes, distance);
+//		}
+//		
+//	}
+//	
+//	
+//}
